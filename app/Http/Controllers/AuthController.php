afd3f12d144bf3be113877b6f34f99e0758f191f
tangguhlaks <tangguhlaksana0@gmail.com>
2024-03-25 20:23:14 +0700

    login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    function login(Request $request){
+        $role = $request->role;
+        $username = $request->username;
+        $password = $request->password;
+        // Cek apakah pengguna dengan username yang diberikan ada dalam tabel
+        $user = User::where('username', $username)->first();
+        if($user) {
+            if(password_verify($password, $user->password)) {
+                $token = $user->createToken('authToken')->plainTextToken;
+                return response()->json(['status_auth' => true,'message'=>'Login Successfuly'], 200);
+            } else {
+                // Password tidak cocok
+                return response()->json(['status_auth' => false,'message'=>'Wrong Password'], 200);
+            }
+        } else {
+            // Pengguna tidak ditemukan
+            return response()->json(['status_auth' => false,'message'=>'User Not Found!'],404);
+        }
+    }
+    
+}
