d48b1f5df4a348c045f8827348bb8bd9bfb53eae
tangguhlaks <tangguhlaksana0@gmail.com>
2024-04-07 13:58:57 +0700

    News API

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\News; // Assuming your News model is located in 'app/Models/News.php'
+
+class NewsController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'image' => 'required|max:5048',
+            'description' => 'required|string'
+        ]);
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('assets/news'),$imageName);
+            $news = new News();
+            $news->title = $request->title;
+            $news->image = $imageName;
+            $news->description = $request->description;
+            $news->save();
+        } else {
+            return response()->json(['status' => false, 'message' => 'Image is required'], 400);
+        }
+       
+        // Redirect to a specified route or action
+        return response()->json(['status' => true,'message'=>'News Success Created'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {            
+            // Find the News by its id
+            $news = News::findOrFail($id);
+
+            // Return the view with the News data
+            return response()->json(['status' => true,'data'=>$news], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => false,'data'=>$news], 404);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function showAll()
+    {
+        try {
+            // Find the News by its id
+            $news = News::all();
+
+            // Return the view with the News data
+            return response()->json(['status' => true,'data'=>$news], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => false,'data'=>$news], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Find the News by its id
+        $news = News::findOrFail($id);
+
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'image' => 'sometimes|max:5048',
+            'description' => 'required|string'
+        ]);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('assets/news'),$imageName);
+            // Delete previous image if exists
+            unlink(public_path('assets/news/'.$news->image));
+            $news->image = $imageName;
+        }
+        $news->title = $request->title;
+        $news->description = $request->description;
+        $news->save();
+        
+        return response()->json(['status' => true,'message'=>'News Success Updated'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Find the News by its id
+        $news = News::findOrFail($id);
+    
+        // Delete the associated image file if it exists
+        if ($news->image) {
+            unlink(public_path('assets/news/'.$news->image));
+        }
+    
+        // Delete the News
+        $news->delete();
+    
+        // Return a JSON response indicating success
+        return response()->json(['status' => true, 'message' => 'News Successfully Deleted'], 200);
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -25,6 +26,13 @@ Route::get('/report/{id}', [ReportController::class, 'show']);
 Route::get('/report', [ReportController::class, 'showAll']);
 Route::put('/report/{id}', [ReportController::class, 'update']);
 Route::delete('/report/{id}', [ReportController::class, 'destroy']);
+
+
+Route::post('/news', [NewsController::class, 'store']);
+Route::get('/news/{id}', [NewsController::class, 'show']);
+Route::get('/news', [NewsController::class, 'showAll']);
+Route::post('/news-update/{id}', [NewsController::class, 'update']);
+Route::delete('/news/{id}', [NewsController::class, 'destroy']);
 
 
 Route::post('/user-by-username', [UserController::class, 'getUserByUsername']);
